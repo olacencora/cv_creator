3c4d6dce3585a21ab17390f3ca953ddd11ee8d87
olacencora <52131675+olacencora@users.noreply.github.com>
2020-04-23 20:24:22 +0200

    next and prev butons working - js

--- cv_creator-main.js
+++ cv_creator-main.js
@@ -3,16 +3,40 @@ let prevButtons = document.getElementsByClassName("prev");
 
 console.log(nextButtons);
 
-function findParentPage() {
+function findParentPageOnNext() {
   console.log("test");
   let parentPage = this.parentElement.parentElement;
   let parentPageId = parentPage.id;
   console.log(parentPageId);
   let parentPageNum = parseInt(parentPageId.substr(1));
   console.log(parentPageNum);
+  let nextPageId = parentPageNum + 1;
+  let nextPage = document.getElementById("s" + nextPageId);
+  console.log(nextPage);
+
+  parentPage.style.display = "none";
+  nextPage.style.display = "block";
+}
+
+function findParentPageOnPrev() {
+  console.log("test");
+  let parentPage = this.parentElement.parentElement;
+  let parentPageId = parentPage.id;
+  console.log(parentPageId);
+  let parentPageNum = parseInt(parentPageId.substr(1));
+  console.log(parentPageNum);
+  let prevPageId = parentPageNum - 1;
+  let prevPage = document.getElementById("s" + prevPageId);
+  console.log(prevPage);
+
+  parentPage.style.display = "none";
+  prevPage.style.display = "block";
 }
 
 for (let i = 0; i < nextButtons.length; i++) {
-  console.log("test click");
-  nextButtons[i].addEventListener("click", findParentPage);
+  nextButtons[i].addEventListener("click", findParentPageOnNext);
+}
+
+for (let i = 0; i < prevButtons.length; i++) {
+  prevButtons[i].addEventListener("click", findParentPageOnPrev);
 }
