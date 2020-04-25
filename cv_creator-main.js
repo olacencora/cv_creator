17772e0337c44df22c1509e16db572a14565338c
olacencora <52131675+olacencora@users.noreply.github.com>
2020-04-25 14:52:40 +0200

    js - select - finish all mechanism

--- cv_creator-main.js
+++ cv_creator-main.js
@@ -68,12 +68,65 @@ for (let i = 0; i < customSelect.length; i++) {
   createdDivOption = document.createElement("div");
   createdDivOption.setAttribute("class", "select-items select-hide");
 
-  for (j = 0; j < selectElement.length; j++) {
+  for (let j = 0; j < selectElement.length; j++) {
     // for each option in old select, create new list of options
     createdDivOptionList = document.createElement("div");
     createdDivOptionList.innerHTML = selectElement.options[j].innerHTML;
     createdDivOptionList.addEventListener("click", function (e) {
       // when you click on elelement from list, change text in select
+      var y, k;
+      let s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+
+      let h = this.parentNode.previousSibling;
+
+      for (let i = 0; i < s.length; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+          s.selectedIndex = i;
+          h.innerHTML = this.innerHTML;
+          y = this.parentNode.getElementsByClassName("same-as-selected");
+          for (let k = 0; k < y.length; k++) {
+            y[k].removeAttribute("class");
+          }
+          this.setAttribute("class", "same-as-selected");
+          break;
+        }
+      }
+      h.click();
     });
+    createdDivOption.appendChild(createdDivOptionList);
+  }
+  customSelect[i].appendChild(createdDivOption);
+
+  createdDivSelect.addEventListener("click", function (e) {
+    /* when select is click - close others options and open this one:*/
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+except the current select box:*/
+  var y,
+    arrNo = [];
+  let selectItems = document.getElementsByClassName("select-items");
+  let selectSelected = document.getElementsByClassName("select-selected");
+  for (let i = 0; i < selectSelected.length; i++) {
+    if (elmnt == selectSelected[i]) {
+      arrNo.push(i);
+    } else {
+      // remove class to show arrow
+      selectSelected[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (let i = 0; i < selectItems.length; i++) {
+    if (arrNo.indexOf(i)) {
+      // add class to hide
+      selectItems[i].classList.add("select-hide");
+    }
   }
 }
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
